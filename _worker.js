0cf9ee49515c6319822e131ec6d5c5730e649826
xyrjok <yyl9821@163.com>
2025-11-26 23:12:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -987,7 +987,7 @@ async function handleApi(request, env, url) {
                      biz_content: JSON.stringify({
                          out_trade_no: order.id,
                          total_amount: order.total_amount,
-                         subject: `HLTX${order.id}` // 合并订单会显示 “购物车合并订单” 商品名称是：subject: `${order.product_name}`
+                         subject: `HLTX订单号：${order.id}` // 合并订单会显示 “购物车合并订单” 商品名称是：subject: `${order.product_name}`
                      })
                  };
                  params.sign = await signAlipay(params, config.private_key);
