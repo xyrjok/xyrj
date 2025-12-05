74e8303118490a245c041bc0c0bfc80fe03d3cb9
xyrjok <yyl9821@163.com>
2025-12-05 17:31:50 +0800

    Update product-page.js

--- themes/default/files/product-page.js
+++ themes/default/files/product-page.js
@@ -771,7 +771,7 @@ function initStickySidebar() {
 
     // 2. 配置参数
     const options = {
-        topSpacing: 60,             // 距离顶部 80px
+        topSpacing: 80,             // 距离顶部 80px
         bottomSpacing: 20,          // 距离底部 20px
         containerSelector: '.product-detail-grid', // 外层容器
         innerWrapperSelector: '.sidebar-inner'     // 内层移动元素
